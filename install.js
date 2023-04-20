18707ae1ebe0f7de8056076d1a27844a1ac452a0
unknown <sysu.peter.hsu@gmail.com>
2023-04-20 16:08:59 +0800

    feat: download instead of gyp build

---
+++ install.js
@@ -0,0 +1,81 @@
+const { promises: fs } = require('fs');
+const path = require('path');
+const util = require('util');
+const ora = require('ora');
+const download = require('download');
+const rimraf = require('rimraf');
+const throttle = require('lodash.throttle');
+const si = require('systeminformation');
+
+const CWD = process.cwd();
+const BUILD_FOLDER = path.join(CWD, 'build');
+const RELEASE_FOLDER = path.join(CWD, 'build/Release');
+
+const DIST_URL = 'https://www.xupea.com/robotjs';
+// const SUPPORTED_PLATFORM = ['darwin', 'win32', 'ubuntu', 'kylin', 'uos'];
+// const SUPPORTED_ARCH = ['x64', 'x86', 'arm64', 'mips64el'];
+
+async function getRobotjsPath() {
+  const cpu = await si.cpu();
+  const osInfo = await si.osInfo();
+
+  if (osInfo.platform === 'linux') {
+    if (cpu.arch === 'x64' && cpu.vendor === 'Hygon') {
+        return `${osInfo.distro}-${cpu.arch}-Hygon`;
+    } else if (cpu.arch === 'mips64el') {
+        return `${osInfo.distro}-${cpu.arch}-Loongson`;
+    }
+  }
+
+  console.log(`${process.platform}-${process.arch}`)
+
+  return `${process.platform}-${process.arch}`;
+}
+
+function getAssetUrl(path) {
+  return `${DIST_URL}/${path}/robotjs.node`;
+}
+
+async function downloadArtifact(source, dest) {
+  const spinner = ora(`Downloading ${source}`).start();
+  // https://github.com/kevva/decompress-unzip/issues/12
+  await download(source, dest, {
+    extract: true,
+    map: (file) => {
+      if (file.type === "file" && file.path.endsWith("/")) {
+        file.type = "directory";
+      }
+      return file;
+    },
+  }).on(
+    "downloadProgress",
+    throttle(({ percent, transferred, total }) => {
+      spinner.info(
+        `Downloading progress ${(percent * 100).toFixed(
+          2
+        )}% ${transferred}/${total}kb ${source} `
+      );
+    }, 500)
+  );
+  spinner.succeed(`Downloaded ${source}`);
+}
+
+async function install() {
+//   if (!SUPPORTED_PLATFORM.includes(os.platform())) {
+//     throw Error(`Platform: ${os.platform()} is not supported!`);
+//   }
+//   if (!RTM_ARTIFACTS[`${os.platform()}-${arch}`]) {
+//     throw Error(`Arch: ${arch} is not supported!`);
+//   }
+  await util.promisify(rimraf)(BUILD_FOLDER);
+  await fs.mkdir(BUILD_FOLDER);
+  await fs.mkdir(RELEASE_FOLDER);
+
+  const robotjsAssetUrl = getAssetUrl(await getRobotjsPath());
+  await downloadArtifact(robotjsAssetUrl, RELEASE_FOLDER);
+}
+
+install().catch((error) => {
+  console.error(error);
+  process.exit(-1);
+});
